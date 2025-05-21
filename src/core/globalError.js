fba5d20e721e6de0eda22e8b95c4b7002ab4cb5b
usman.backend.dev <usman@asztechnologies.com>
2025-05-21 12:24:01 +0530

    global error issue fixed

--- src/core/globalError.js
+++ src/core/globalError.js
@@ -1,7 +1,32 @@
-// Package Imports
-
 // Custom Imports
-import * as responser from "./responser.js"
+import * as responser from "./responser.js";
+import AppError from "./appError.js";
+import * as logger from "../utils/log.js";
+
+// statusCode, handler, messageCode, optionalMessage = null
+const handleCastErrorDB = (err) => {
+    const message = `${err.path}: ${err.value}.`;
+    return new AppError(400, "Invalid Casting", message, true);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+    let message = err.errmsg.match(/(["'])(\\?.)*?\1/);
+    if (message) {
+        message = message[0];
+    }
+    logger.data("handleDuplicateFieldsDB regex value", message);
+    return new AppError(400, "Duplicate field value", message, true);
+};
+
+const handleValidationErrorDB = (err) => {
+    const errors = Object.values(err.errors).map((el) => el.message);
+    const message = errors.join(". ");
+    return new AppError(400, "Invalid input data", message, true);
+};
+
+const handleJWTError = () => new AppError(401, "Invalid token. Please log in again!");
+
+const handleJWTExpiredError = () => new AppError(401, "Your token has expired! Please log in again.");
 
 const sendError = (err, req, res) => {
     if (err.isOperational) {
@@ -19,6 +44,11 @@ const errorHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
     let error = err;
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError") error = handleValidationErrorDB(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
+    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
     sendError(error, req, res);
 };
 
